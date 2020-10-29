d3b3b2f8a18df3c04d93bcb6ba4b3a0f30ac8c30
Fernando <fmoya97.fm@gmail.com>
2020-10-29 17:55:07 -0300

    update: login validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,8 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class LoginComponent implements OnInit {
 
   formularioLogin: FormGroup;
+  datosCorrectos: boolean = true;
+  textoError: string = '';
   constructor(private fb: FormBuilder, public serviceAuth: AngularFireAuth) { }
 
   ngOnInit(): void {
@@ -22,9 +24,20 @@ export class LoginComponent implements OnInit {
   }
 
   ingresar(): void {
-    this.serviceAuth.signInWithEmailAndPassword(this.formularioLogin.value.email, this.formularioLogin.value.password).then((user) => {
-      console.log(user);
-    })
+    if (this.formularioLogin.valid) {
+      this.datosCorrectos = true;
+      this.serviceAuth.signInWithEmailAndPassword(this.formularioLogin.value.email, this.formularioLogin.value.password).then((user) => {
+        console.log(user);
+      }).catch((error) => {
+        //cuando el usuario no existe
+        this.datosCorrectos = false;
+        this.textoError = error.message;
+      });
+    } else {
+      this.datosCorrectos = false;
+      this.textoError = 'Revisar que los datos esten correctos'
+    }
+
   }
 
 }
